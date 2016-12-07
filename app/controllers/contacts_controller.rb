181fc5487415d8d45e6ef4dc639c697c9aa13f44
Myles Berueda <myles.berueda@gmail.com>
2016-12-07 12:05:44 +0000

    Added comments to contacts controller, view, and routes files.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,10 @@
 class ContactsController < ApplicationController
+    # Defines a new method (and pulls corresponding view) while instantiating a new object (Contact model) for each page view from different get requests.
     def new
         @contact = Contact.new
     end
     
+    # Defines a new method to create (contacts#create POST request) with parameters. Checks contact model for code (in this case, validations) and tries to save to db once submit button is clicked.
     def create
        @contact = Contact.new(contact_params)
         if @contact.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new', as: 'new_contact' # Added as just to demonstrate that you can change the routes file instead of changing the whole website's code to fix links.
+    root to: 'pages#home' # root to path denoting the root directory of a website (www.example.com as opposed to www.example.com/home) showing the home view from the pages controller.
+    get 'about', to: 'pages#about' # Route that gets about requests and pulls the about view in the pages controller.
+    resources :contacts, only: :create # Demonstrating the use of resources when creating routes to a corresponding controller, and limiting the amount of routes created with either an array or single verb.
+    get 'contact-us', to: 'contacts#new', as: 'new_contact' # Added 'as' just to demonstrate that you can change the routes file instead of changing the whole website's code to fix links.
 end
