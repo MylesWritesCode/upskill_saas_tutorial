ee094dc33eb85b64b3d79fadeaa0c4a71090540f
Myles Berueda <myles.berueda@gmail.com>
2016-12-07 11:54:31 +0000

    Changed routes file to demonstrate 'as' modifier.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
-    get 'contact-us', to: 'contacts#new'
+    get 'contact-us', to: 'contacts#new', as: 'new_contact' # Added as just to demonstrate that you can change the routes file instead of changing the whole website's code to fix links.
 end
