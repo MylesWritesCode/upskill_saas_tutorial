d8cdfd2e1a4d23293b761862e3b632ee135d2512
Myles Berueda <myles.berueda@gmail.com>
2016-12-12 08:04:36 +0000

    Added profiles controller create action.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,7 +14,6 @@ class ContactsController < ApplicationController
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success]= "Message sent."
             redirect_to new_contact_path
-
         else
             # The following code is trying to get all the error messages to show.
             # @contact.errors.each do |msg|
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,33 @@ class ProfilesController < ApplicationController
   
   # GET request to /users/:user_id/profile/new
   def new
+    # Render new blank profile detail form.
     @profile = Profile.new
   end
+  
+  # POST request to /users/:user_id/profile
+  def create
+    # First grabs the user id from the parameters on the page based on login.
+    @user = User.find( params[:user_id] )
+    # We're using @user.build_profiles, so that the form is tied to the user that's logged in.
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(
+        :first_name, 
+        :last_name, 
+        :job_title, 
+        :phone_number, 
+        :contact_email, 
+        :description
+      )
+    end
 end
